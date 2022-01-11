1f4ab8c922e4caeecd0a6f1e01f837ffe94fcf85
Aniket Katkar <51777795+aniketkatkar97@users.noreply.github.com>
2022-01-11 23:33:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,115 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import DogImages from "./components/DogImages";
+import "./App.css";
+import axios from "axios";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
+  //Setting states for the app
+  const [breeds, setBreeds] = useState([]);
+  const [filteredBreeds, setFilteredBreeds] = useState([]);
+  const [filterResults, setFilterResults] = useState(0);
 
-  handleClick = api => e => {
-    e.preventDefault()
+  //fetching the information about all the dog breeds at the START
+  useEffect(() => {
+    fetchBreedsInfo();
+    // eslint-disable-next-line
+  }, []);
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  //Function to fetch the information about all the dog breeds
+  const fetchBreedsInfo = () => {
+    axios
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then((data) => {
+        mapDataForBreeds(data); //Getting details for every breed
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-  render() {
-    const { loading, msg } = this.state
+  //function to map all the data for each breed
+  const mapDataForBreeds = async (data) => {
+    let breeds_list = [];
+    for (let breed in data.data.message) {
+      let breedData = { name: breed };
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+      //adding images to breed data
+      let imageData = await mapImagesForBreeds(breed); //getting image for the breed
+      if (imageData.data.message.length !== 0) {
+        breedData.img = imageData.data.message;
+      }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+      //adding sub breeds to breed data
+      let subBreeds = await mapSubBreeds(breed); //getting sub breeds for the breed
+      breedData["subbreeds"] = subBreeds;
+
+      breeds_list.push(breedData);
+    }
+    setBreeds(breeds_list); //Saving all the information in "breeds" state
+  };
+
+  //function to fetch and map the images for given breeds
+  const mapImagesForBreeds = async (breed) => {
+    let imageData = await axios.get(
+      `https://dog.ceo/api/breed/${breed}/images/random/4`
+    );
+    return imageData;
+  };
+
+  //function to fetch and map information about sub breeds for a given breed
+  const mapSubBreeds = async (breed) => {
+    let sub_breed_list = [];
+    let list = await axios.get(`https://dog.ceo/api/breed/${breed}/list`);
+    if (list.data.message.length !== 0) {
+      for (let subBreed of list.data.message) {
+        let subBreedData = { name: subBreed };
+        let imageData = await axios.get(
+          `https://dog.ceo/api/breed/${breed}/${subBreed}/images/random`
+        );
+        if (imageData.data.message.length !== 0) {
+          subBreedData.img = imageData.data.message;
+        }
+
+        sub_breed_list.push(subBreedData);
+      }
+      return sub_breed_list;
+    }
+  };
+
+  //onchange handler for filter search input
+  const handleChange = (e) => {
+    let filter = e.target.value.trim();
+    let newList = breeds.filter((breed) => breed.name === filter);
+    setFilterResults(newList.length);
+    setFilteredBreeds(newList);
+  };
+
+  return (
+    <div>
+      <Navbar breeds={breeds} />
+      <div className="container">
+        <div className="row g-4 my-3 filter-form">
+          <div className="col-auto">
+            <input
+              type="text"
+              className="form-control input"
+              id="breedName"
+              placeholder="Type here to filter by breed"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        {breeds.length === 0 ? (
+          <h1>Please wait while we fetch the data from the server...</h1>
+        ) : filterResults === 0 ? (
+          <DogImages breeds={breeds} />
+        ) : (
+          <DogImages breeds={filteredBreeds} />
+        )}
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default App
+export default App;
